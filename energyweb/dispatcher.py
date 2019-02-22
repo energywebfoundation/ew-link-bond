c654cd5d556d55fafe7a21ba2b41b9f3cb2b9289
Robert Sprunk <47247235+renesy@users.noreply.github.com>
2019-02-22 17:35:33 +0100

    Improve code quality

--- energyweb/dispatcher.py
+++ energyweb/dispatcher.py
@@ -4,15 +4,14 @@ Asynchronous event watcher loop
 import asyncio
 import datetime
 import time
-import urllib.request, urllib.error
 import concurrent
 
 class Task:
     """
-    Tasks are routines that run from time to time respecting an interval and spawn sync or async coroutines.
+    Tasks are routines that run from time to time respecting an interval and spawn coroutines.
     These routines may only execute if a trigger condition is fired.
     """
-    def __init__(self, polling_interval: datetime.timedelta, eager: bool=False):
+    def __init__(self, polling_interval: datetime.timedelta, eager: bool = False):
         """
         :param polling_interval: in seconds
         """
@@ -20,24 +19,36 @@ class Task:
         self.eager = eager
 
     def prepare(self):
-        pass
+        """
+        Perform steps required prior to running the main task
+        run exactly once
+        """
 
     def main(self) -> bool:
-        return True
+        """
+        The main task
+        run until it returns False
+        """
 
     def finish(self) -> None:
-        pass
+        """
+        Perform steps required after running the main task
+        run exactlt once
+        """
 
     def run(self, *args):
+        """
+        run all steps of the task
+        """
         self.prepare()
-        while self.main(*args):
+        while self.main(*args) is True:
             # sleep yields to the thread scheduler
             time.sleep(self.polling_interval.total_seconds())
         self.finish()
 
 class EventLoop:
     """
-    Delegate and run a set of Tasks in loop that handles I/O or computational blocking with async calls.
+    Delegate and run a set of Tasks in loop that handles I/O or blocking with async calls.
     https://en.wikipedia.org/wiki/Event_loop
     https://docs.python.org/3/library/asyncio.html
     """
@@ -46,11 +57,17 @@ class EventLoop:
         self.started_tasks = []
 
     def add_task(self, task: Task, *args):
+        """
+        add task to event task list
+        """
         if not task:
             raise Exception('Please add a Task type with callable task named method.')
         self.task_list.append((task, args))
 
     async def run(self):
+        """
+        execute all tasks in task list in their own thread
+        """
         loop = asyncio.get_running_loop()
         with concurrent.futures.ThreadPoolExecutor() as pool:
             for (task, args) in self.task_list:
@@ -64,17 +81,16 @@ class App(EventLoop):
     General application abstraction
     """
 
-    def prepare(self):
-        pass
-
     def configure(self):
+        """
+        Overwrite this method to implement application logic
+        add tasks here
+        """
         raise NotImplementedError
 
-    def finish(self):
-        pass
-
     def run(self):
-        self.prepare()
+        """
+        Call the overwritten configure and run the eventLoop
+        """
         self.configure()
         asyncio.run(super().run())
-        self.finish()
