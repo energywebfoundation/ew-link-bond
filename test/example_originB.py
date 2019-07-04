129fdd83b355dc9819c3a62b2c48b97f5fe5ad1d
Robert Sprunk <47247235+renesy@users.noreply.github.com>
2019-07-04 14:59:46 +0200

    Get address from lookup interface

--- test/example_originB.py
+++ test/example_originB.py
@@ -13,39 +13,67 @@ class Example:
         self.SECONDS_BETWEEN_RETRIES = 1
 
         client_url = "http://localhost:8545"
-        wallet_add = "0x68F89f072A37c9571733E899EFF7219a03d51bb0"
-        wallet_pwd = "f97120b63afc4a6c622a4131687ac6d3ef068590beec04276a3d13bc4148c12b"
-
-        # npm run start-test-backend:
-        # GET - ProducingAsset 0 (contract 0xa82e267ddb7417b1ef2f88378bae35da5ff33647)
-        # PUT - OriginContractLookupMarketLookupMapping 0xe377ae7241f76ac5cdb89f5debc0dbe82f9c2c03
 
         # npm start:
         # info: Asset Contract Deployed: 0x6389D7786F7B2cd7e951d417CAD7e5e04295ae48
-        contract_address = "0x6389D7786F7B2cd7e951d417CAD7e5e04295ae48"
+        contract_address = "0xb19B5d529F8af11f5b91d4FC9B33106844F210a0"
 
-        with open('AssetProducingDB.json') as contract_json:
+        with open('AssetContractLookupInterface.json') as contract_json:
             contract = json.load(contract_json)
-
-        #print(contract['abi'])
 
         self.w3 = web3.Web3(web3.HTTPProvider(client_url))
 
         contract_instance = self.w3.eth.contract(
             abi=contract['abi'],
             address=self.w3.toChecksumAddress(contract_address))
-            #bytecode=contract['bytecode'])
 
-        method_name = "getLastSmartMeterReadWh"
-        asset_id = 0
-        args = [asset_id]
-        
-        #smartMeterRead = getattr(contract_instance.functions, method_name)(*args).call()
-        smartMeterRead = contract_instance.functions.getLastSmartMeterReadWh(0).call()
+        assetProducingRegistryLogic = contract_instance.functions.assetProducingRegistry().call()
+        print(assetProducingRegistryLogic)
+        # works: 0x944655d2D4C6C68dBb2B1576A89fAD5e36e714dA
 
-        # with correct method name -> Error: VM Exception while processing transaction: revert ganache-core.node.cli.js:10
+        # TODO: get assetProducingDB address from assetProducingRegistryLogic
 
-        # with incorrect method name -> Error: The function 'getAssetLocation' was not found in this contract's abi. ", 'Are you sure you provided the correct contract abi?
+        assetProducingDB = "0xcab91029732e642067F9095dbb395937d67667F7"
+
+        with open('AssetProducingDB.json') as contract_json:
+            contract = json.load(contract_json)
+
+        self.contract = contract
+
+        contract_instance = self.w3.eth.contract(
+            abi=contract['abi'],
+            address=self.w3.toChecksumAddress(assetProducingDB))
+
+
+        wallet_add = "0x343854a430653571b4de6bf2b8c475f828036c30"
+        wallet_pwd = "12c5c7473dbdb92a524a93baa14ded529fe29acef8d269a3901c14a15e2b0f98"
+        private_key = bytearray.fromhex(wallet_pwd)
+        nonce = self.w3.eth.getTransactionCount(account=self.w3.toChecksumAddress(wallet_add))
+        transaction = {
+            'from': self.w3.toChecksumAddress(wallet_add),
+            'gas': 400000,
+            'gasPrice': self.w3.toWei('0', 'gwei'),
+            'nonce': nonce,
+        }
+
+        method_name = "setLastSmartMeterReadWh"
+
+        tx = contract_instance.functions.setLastSmartMeterReadWh(0, 100000).buildTransaction(transaction)
+
+        print(tx)
+
+        signed_txn = self.w3.eth.account.signTransaction(tx, private_key=private_key)
+
+        tx_hash = self.w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+
+    def getContractMethod(self, method_name):
+        for method in self.contract['abi']:
+            try:
+                if method['name'] == method_name:
+                    return method
+            except KeyError:
+                continue
 
 def main():
     e = Example()
